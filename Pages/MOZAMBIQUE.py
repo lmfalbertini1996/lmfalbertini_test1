2029df8e4cb0b9b0d69f1e3618d7ad4698224952
lmfalbertini1996 <109661090+lmfalbertini1996@users.noreply.github.com>
2022-08-30 17:00:02 +0200

    Delete Pages directory

--- Pages/MOZAMBIQUE.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-import altair as alt
-import webbrowser
-from PIL import Image
-
-st.title("Partners")
-
-st.markdown(' #Politecnico di Milano has engaged with different partners in order to create pro-active partnerhips')
