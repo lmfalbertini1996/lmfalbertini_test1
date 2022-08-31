e218eb8aa583a5ff07d504c7016d2584c2592d90
lmfalbertini1996 <109661090+lmfalbertini1996@users.noreply.github.com>
2022-08-31 11:05:35 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -87,13 +87,77 @@ For more info and statistics, please click on the button "SDG 7".
 url4 = 'sdgs.un.org/goals/goal7'
 if st.button('SDG 7'):
     webbrowser.open_new_tab(url4)
+    
+ st.write("""***""")
+    
+    st.title("PARTNERS")
 
+col1, col2 = st.columns([5,2])
+with col1:
+    st.write("""
+            ##  FONDAZIONE CARIPLO
+            
+             """)
+with col2:
+    image = Image.open('FONDAZIONECARIPLO.png')
+    st.image(image, use_column_width=True)
+
+st.write("""
+       "For 30 years, Fondazione Cariplo has been promoting the life of communities, supporting those who operate in the area and are closest to people's needs. The distances within our communities are increasing, making them more fragmented and fragile. Today more than ever, however, there is a need to shorten these distances in order to have strong and inclusive communities, to support people's lives and to have strong institutions capable of reconciling different needs and directing resources and choices towards a better future for all and in which all can recognise themselves"
+               """)
+
+url4 = 'https://www.fondazionecariplo.it/'
+if st.button('FONDAZIONE CARIPLO'):
+    webbrowser.open_new_tab(url4)
+    
+st.write("""***""")
+  
+col1, col2 = st.columns([5,2])
+with col1:
+    st.write("""
+            ##  ICEI
+            
+             """)
+with col2:
+    image = Image.open('ICEI.png')
+    st.image(image, use_column_width=True)
+
+st.write("""
+       "We work with people and local communities to improve social and economic conditions and to promote inclusive, fair and sustainable societies with a participatory approach.
+We implement cooperation programmes in Italy and across the world, with special attention to environment, in the areas of intercultural citizenship, labour market integration, sustainable agriculture and responsible tourism.
+Priority targets across all our focus areas are vulnerable people, in particular youth and women"
+               """)
+
+url5 = 'http://icei.it/'
+if st.button('ICEI'):
+    webbrowser.open_new_tab(url5)
+    
 st.write("""***""")
 
-discourse_url = "discuss.streamlit.io"
-topic_id = 8061
+col1, col2 = st.columns([5,2])
+with col1:
+    st.write("""
+            ##  ENGREEN
+            
+             """)
+with col2:
+    image = Image.open('ENGREEN.png')
+    st.image(image, use_column_width=True)
 
-st_discourse(discourse_url, topic_id)
+st.write("""
+       "Our Mission is to build a long-lasting business relationship based on trust and competency in order to achieve tangible results on energy access, low-carbon technologies and climate change mitigation through innovative business models to de-risk investments"
+       """)
+
+url6 = 'https://www.engreensolutions.com/about/'
+if st.button('ENGREEN'):
+    webbrowser.open_new_tab(url6)
+    
+st.write("""***""")
+
+#discourse_url = "discuss.streamlit.io"
+#topic_id = 8061
+
+#st_discourse(discourse_url, topic_id)
 
 st.write("""
         ## Contact us
