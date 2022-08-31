057f4a028ccd6d39838c7b70915657ee4b45ff6c
lmfalbertini1996 <109661090+lmfalbertini1996@users.noreply.github.com>
2022-08-31 10:52:48 +0200

    Update PARTNERS.py

--- pages/PARTNERS.py
+++ pages/PARTNERS.py
@@ -8,9 +8,16 @@ st.title("PARTNERS")
 
 st.write("""***""")
 
-st.write("""
-        ## FONDAZIONE CARIPLO
-         """)
+
+col1, col2 = st.columns([5,2])
+with col1:
+    st.write("""
+            ##  FONDAZIONE CARIPLO
+            
+             """)
+with col2:
+    image = Image.open('FONDAZIONECARIPLO.png')
+    st.image(image, use_column_width=True)
 
 st.write("""
        "For 30 years, Fondazione Cariplo has been promoting the life of communities, supporting those who operate in the area and are closest to people's needs. The distances within our communities are increasing, making them more fragmented and fragile. Today more than ever, however, there is a need to shorten these distances in order to have strong and inclusive communities, to support people's lives and to have strong institutions capable of reconciling different needs and directing resources and choices towards a better future for all and in which all can recognise themselves"
@@ -21,10 +28,16 @@ if st.button('FONDAZIONE CARIPLO'):
     webbrowser.open_new_tab(url4)
     
 st.write("""***""")
-    
-st.write("""
-        # ICEI
-         """)
+  
+col1, col2 = st.columns([5,2])
+with col1:
+    st.write("""
+            ##  ICEI
+            
+             """)
+with col2:
+    image = Image.open('ICEI.png')
+    st.image(image, use_column_width=True)
 
 st.write("""
        "We work with people and local communities to improve social and economic conditions and to promote inclusive, fair and sustainable societies with a participatory approach.
@@ -40,9 +53,15 @@ if st.button('ICEI'):
     
 st.write("""***""")
 
-st.write("""
-        # ENGREEN
-         """)
+col1, col2 = st.columns([5,2])
+with col1:
+    st.write("""
+            ##  ENGREEN
+            
+             """)
+with col2:
+    image = Image.open('ENGREEN.png')
+    st.image(image, use_column_width=True)
 
 st.write("""
        "Our Mission is to build a long-lasting business relationship based on trust and competency in order to achieve tangible results on energy access, low-carbon technologies and climate change mitigation through innovative business models to de-risk investments"
