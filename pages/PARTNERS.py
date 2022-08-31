13e508bab2610336e6fde258ac7365a183486f5c
lmfalbertini1996 <109661090+lmfalbertini1996@users.noreply.github.com>
2022-08-31 10:13:36 +0200

    Update PARTNERS.py

--- pages/PARTNERS.py
+++ pages/PARTNERS.py
@@ -5,3 +5,19 @@ import webbrowser
 from PIL import Image
 
 st.title("PARTNERS")
+
+col1, col2, col3 = st.columns([5,5,1])
+
+with col1:
+    url1 = '[FONDAZIONE CARIPLO](https://www.fondazionecariplo.it/)'
+    st.markdown(url1, unsafe_allow_html=True)
+
+with col2:
+    url2 = '[ENGREEN](http://https://www.engreensolutions.com/)'
+    st.markdown(url2, unsafe_allow_html=True)
+
+with col3:
+    url3 = '[RESEARCH](http://icei.it/)'
+    st.markdown(url3, unsafe_allow_html=True)
+
+st.write("""***""")
